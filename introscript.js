ecc5e50ad8dee9fa556839de26bc091a8c303784
mscbr <scibor.maciej@gmail.com>
2018-08-23 22:49:13 +0000

    [Intro page final design with all intro and outro animations designed]

--- introscript.js
+++ introscript.js
@@ -1,5 +1,5 @@
-
-var showEnter = function() {
+//intro-circle button functions setup
+var setButton = function() {
 		//getting DOM elements
 		var enterTitle = document.getElementsByClassName("enter")[0];
 		var innerCircle = document.getElementsByClassName("inner-circle")[0];
@@ -10,10 +10,25 @@ var showEnter = function() {
 		function visHidden() {
 			enterTitle.style.visibility = "hidden";
 		}
-		//executing hide/show when hover on .inner-circle
+		//background fade to white function
+		function changeBackground() {
+			var introBody = document.getElementsByTagName("body")[0];
+			innerCircle.style.animationName = "toWhite";
+			innerCircle.style.animationDuration = "2s";
+			innerCircle.style.animationIterationCount = "1";
+			innerCircle.style.animationFillMode = "forwards";
+			introBody.classList.add("page-fadeout");
+		}
+		//add event listeners
 		innerCircle.addEventListener("mouseover", visVisible);
 		innerCircle.addEventListener("mouseout", visHidden);
+		innerCircle.addEventListener("click", changeBackground);
+}
+$(document).ready(setButton);
+
+//function that delays a href to index.html
+var delay = function(url) {
+	setTimeout( function() { window.location = url }, 2300);
 }
 
 
-$(document).ready(showEnter);
