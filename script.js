4a8e1dc58dcfe30bda2bae0f4a53fb09972dac58
mscbr <scibor.maciej@gmail.com>
2018-08-29 10:21:34 +0000

    [Button triangle icon position functionality added]

--- script.js
+++ script.js
@@ -9,12 +9,15 @@ function iconTab() {
 
 	//function that clears display class from all content-box 
 	//and then add display class according to clicked button
+	//it also refreshes and add button icon accordingly
 	function addClass(buttonNum) {
 		for (var i = 0; i < buttonLength; i++) {
 			document.getElementsByClassName("content-box")[i].classList.remove("display-block");
+			document.getElementsByClassName("triangle-icon")[i].classList.remove("display-inline");
 		}
 		document.getElementsByClassName("animated-title")[0].classList.remove("display-block");
 		document.getElementsByClassName("content-box")[buttonNum].classList.add("display-block");
+		document.getElementsByClassName("triangle-icon")[buttonNum].classList.add("display-inline");
 	}
 	//assigning function tu buttons
 	document.getElementsByTagName("button")[0].addEventListener("click", function() {addClass(0);});
